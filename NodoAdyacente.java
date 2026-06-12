ac3d36a863f24a4b33ca2d1672aa2e7c4734688c
AgustinaCarranza <agcarranza@uade.edu.ar>
2026-06-12 18:20:22 -0300

    Add files via upload

---
+++ NodoAdyacente.java
@@ -0,0 +1,9 @@
+public class NodoAdyacente {
+    Pasillos dato;
+    NodoAdyacente siguiente;
+
+    public NodoAdyacente(Pasillos dato){
+        this.dato = dato;
+        this.siguiente = null;
+    }
+}
